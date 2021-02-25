ba2756e85768b5e079cfde8e8d5777a0eca4d99e
kur0yan <rithvikmarri@live.com>
2021-02-25 01:59:40 -0600

    Edited all three files

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value
+    var years = document.getElementById("years").value
+    var interest = (principal * years * rate )/100
+    var returnval = interest
+    var d = new Date()
+    var currentYear = d.getFullYear()
+    futureYear = parseInt(currentYear) + parseInt(years)
+
+    if (principal<=0){
+        alert("Enter a positive number")
+        document.getElementById("principal").focus()
+        document.getElementById("result").innerHTML="";
+        return    
+    }
+
+    var result = "If you deposit <mark>" + principal + "</mark>,<br\>" + "at an interest rate of <mark>" + rate + "</mark>%.<br/>" + "You will receive an amount of <mark>" + returnval + "</mark>,<br/>" + "in the year <mark>"+ futureYear +"</mark>"
+    document.getElementById("result").innerHTML= result
+
+}
+
+function rangeValue()
+{
+    document.getElementById("rangeval").textContent = document.getElementById("rate").value + '%'
 }
         
